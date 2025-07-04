ddbb6dc45f12e46f2626b29d847e252439a5a66f
zermzerm <zerm0907@gmail.com>
2025-07-04 13:03:40 +0900

    네브바 작업

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,22 @@
+import {AlarmIcon} from "../../assets/icons/AlarmIcon";
+import {LoginIcon} from "../../assets/icons/LoginIcon";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-  return <div className={styles.Container}>NavBar</div>;
+  return (
+    <div className={styles.Container}>
+      <div className={styles.LogoWrapper}>
+        <div className={styles.LogoSection}>s</div>
+        <div>SMARTy 번역 시스템</div>
+      </div>
+      <div className={styles.AlarmSection}>
+        <div className={styles.AlarmWrapper}>
+          <AlarmIcon />
+        </div>
+        <div className={styles.AlarmWrapper}>
+          <LoginIcon />
+        </div>
+      </div>
+    </div>
+  );
 }
