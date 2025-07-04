04a6d080529cb8e4f4f3b8a92e4135449b71340e
zermzerm <zerm0907@gmail.com>
2025-07-04 12:42:17 +0900

    사이드바 작업

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,34 @@
+import {GlobeIcon} from "../../assets/icons/GlobeIcon";
+import {SettingIcon} from "../../assets/icons/SettingIcon";
 import styles from "./SideBar.module.css";
 
 export default function SideBar() {
-  return <div className={styles.Container}>SideBar</div>;
+  return (
+    <div className={styles.Container}>
+      <div className={`${styles.SideBarSection} ${styles.firstSection}`}>
+        <GlobeIcon />
+        <p>SMARTok(실시간 번역)</p>
+      </div>
+      <div className={styles.SideBarSection}>
+        <SettingIcon />
+        <p>기준</p>
+      </div>
+      <div className={styles.SideBarSection}>
+        <p className={styles.SideBarIcon}>P</p>
+        <p>계획</p>
+      </div>
+      <div className={styles.SideBarSection}>
+        <p className={styles.SideBarIcon}>D</p>
+        <p>운영</p>
+      </div>
+      <div className={styles.SideBarSection}>
+        <p className={styles.SideBarIcon}>C</p>
+        <p>점검</p>
+      </div>
+      <div className={styles.SideBarSection}>
+        <p className={styles.SideBarIcon}>A</p>
+        <p>분석</p>
+      </div>
+    </div>
+  );
 }
